4e45fdc7fe58c2a0297d62afd2a74684d7d3d440
JtheDroid <fabi.jobo@gmail.com>
2020-07-28 12:40:48 +0200

    Add MirroredAnimation

---
+++ MirroredAnimation.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Animation.h"
+
+class MirroredAnimation : public virtual Animation {
+public:
+    explicit MirroredAnimation(AnimationDisplay *display) : Animation(display) {}
+
+    unsigned int getLedNum() const override {
+        return getFullLedNum() / 2;
+    }
+
+    unsigned int getFullLedNum() const {
+        return Animation::getLedNum();
+    }
+
+protected:
+    void setLed(unsigned int p, Color color) override {
+        Animation::setLed(p, color);
+        Animation::setLed(getFullLedNum() - 1 - p, color);
+    }
+};
