eba26ad86defa73000dc12dddb71be7d10fee3f0
JtheDroid <fabi.jobo@gmail.com>
2020-07-28 13:28:10 +0200

    Add MirroredAnimationDisplay

---
+++ MirroredAnimationDisplay.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Animation.h"
+
+class MirroredAnimationDisplay : public virtual AnimationDisplay {
+private:
+    unsigned int fullLedNum;
+public:
+    explicit MirroredAnimationDisplay(unsigned int ledNum) : AnimationDisplay(ledNum / 2), fullLedNum(ledNum) {}
+
+    void setLed(unsigned int p, Color color) override {
+        setLedImpl(p, color);
+        setLedImpl(fullLedNum - 1 - p, color);
+    }
+};
