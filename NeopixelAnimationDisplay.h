a46e0c445253c0767aadfaa79280448aea459bbd
JtheDroid <fabi.jobo@gmail.com>
2020-08-08 10:04:49 +0200

    Merge NeopixelAnimationDisplay classes using template for Color

--- NeopixelAnimationDisplay.h
+++ NeopixelAnimationDisplay.h
@@ -1,14 +1,14 @@
+#ifdef ADAFRUIT_NEOPIXEL_H
 #pragma once
 
-#ifdef ADAFRUIT_NEOPIXEL_H
 #include "AnimationDisplay.h"
 #include "ColorRGB.h"
 #include "ColorRGBW.h"
 
-class NeopixelAnimationDisplayRGB : public AnimationDisplay<ColorRGB> {
+template <class Color = ColorRGB>
+class NeopixelAnimationDisplay : public AnimationDisplay<Color> {
 public:
-    NeopixelAnimationDisplayRGB(Adafruit_NeoPixel *strip)
-        : AnimationDisplay<ColorRGB>(strip->numPixels()), strip(strip) {}
+    NeopixelAnimationDisplay(Adafruit_NeoPixel *strip) : AnimationDisplay<Color>(strip->numPixels()), strip(strip) {}
 
     void show() override {
         strip->show();
@@ -18,42 +18,31 @@ public:
         strip->clear();
     }
 
-    void setAllLeds(ColorRGB color) override {
-        strip->fill(Adafruit_NeoPixel::Color(color.r, color.g, color.b));
-    }
+    void setAllLeds(Color color);
 
 protected:
-    void setLedImpl(unsigned int p, ColorRGB color) override {
-        strip->setPixelColor(p, color.r, color.g, color.b);
-    }
+    void setLedImpl(unsigned int p, Color color) override;
 
 private:
     Adafruit_NeoPixel *strip;
 };
 
-class NeopixelAnimationDisplayRGBW : public AnimationDisplay<ColorRGBW> {
-public:
-    NeopixelAnimationDisplayRGBW(Adafruit_NeoPixel *strip)
-        : AnimationDisplay<ColorRGBW>(strip->numPixels()), strip(strip) {}
+template <>
+void NeopixelAnimationDisplay<ColorRGB>::setAllLeds(ColorRGB color) {
+    strip->fill(Adafruit_NeoPixel::Color(color.r, color.g, color.b));
+}
 
-    void show() override {
-        strip->show();
-    }
+template <>
+void NeopixelAnimationDisplay<ColorRGBW>::setAllLeds(ColorRGBW color) {
+    strip->fill(Adafruit_NeoPixel::Color(color.r, color.g, color.b, color.w));
+}
 
-    void clear() override {
-        strip->clear();
-    }
-
-    void setAllLeds(ColorRGBW color) override {
-        strip->fill(Adafruit_NeoPixel::Color(color.r, color.g, color.b, color.w));
-    }
-
-protected:
-    void setLedImpl(unsigned int p, ColorRGBW color) override {
-        strip->setPixelColor(p, color.r, color.g, color.b, color.w);
-    }
-
-private:
-    Adafruit_NeoPixel *strip;
-};
+template <>
+void NeopixelAnimationDisplay<ColorRGB>::setLedImpl(unsigned int p, ColorRGB color) {
+    strip->setPixelColor(p, color.r, color.g, color.b);
+}
+template <>
+void NeopixelAnimationDisplay<ColorRGBW>::setLedImpl(unsigned int p, ColorRGBW color) {
+    strip->setPixelColor(p, color.r, color.g, color.b, color.w);
+}
 #endif
