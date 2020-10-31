498f3d9dbf3b7d9e2b6f6f05d3f3c67d4f52929d
JtheDroid <fabi.jobo@gmail.com>
2020-10-31 13:47:07 +0100

    Reformat code
    Animation::setLedsTransition: check if origLength is 0

--- Animation.h
+++ Animation.h
@@ -5,21 +5,23 @@
 #include <chrono>
 
 unsigned long millis() {
-    return (unsigned long) std::chrono::duration_cast<std::chrono::milliseconds>(
-            std::chrono::system_clock::now().time_since_epoch()).count();
+    return (unsigned long)std::chrono::duration_cast<std::chrono::milliseconds>(
+               std::chrono::system_clock::now().time_since_epoch())
+        .count();
 }
 
 #endif
 
-#include "ColorRGB.h"
 #include "AnimationDisplay.h"
+#include "ColorRGB.h"
 
-template<class Color = ColorRGB>
+template <class Color = ColorRGB>
 class Animation {
 private:
     AnimationDisplay<Color> *display;
     unsigned int ledNum;
     unsigned long counter = 0;
+
 public:
     explicit Animation(AnimationDisplay<Color> *display) : display(display), ledNum(display->getLedNum()) {}
 
@@ -41,7 +43,6 @@ public:
         display->show();
     };
 
-
     /** @brief Skip frames if running too slow to keep up with interval
      *
      *  If runTimed is not called fast enough to keep up with the interval, multiple animation steps are executed.
@@ -61,19 +62,19 @@ public:
     virtual ~Animation() = default;
 
 protected:
-    //Set led at position p to color
+    // Set led at position p to color
     virtual void setLed(unsigned int p, Color color) {
         display->setLed(p, color);
     }
 
-    //Set leds from p1 to p2 to color
+    // Set leds from p1 to p2 to color
     void setLeds(unsigned int p1, unsigned int p2, Color color);
 
-    //Set leds from p1 to p2 to transition from color1 to color2
+    // Set leds from p1 to p2 to transition from color1 to color2
     void setLedsTransition(unsigned int p1, unsigned int p2, Color color1, Color color2);
 
-    //Set all leds to color using display method
-    virtual void setAllLeds(Color color){
+    // Set all leds to color using display method
+    virtual void setAllLeds(Color color) {
         display->setAllLeds(color);
     }
 
@@ -89,49 +90,55 @@ protected:
         display->clear();
     }
 
-    //Set pixel colors using setLed
+    // Set pixel colors using setLed
     virtual void animationStep() = 0;
 
 private:
     bool skipFrames{true};
 };
 
-template<class Color>
+template <class Color>
 void Animation<Color>::run(bool showOnRun) {
     animationStep();
-    if (showOnRun && display->isShowOnRun()) show();
+    if (showOnRun && display->isShowOnRun())
+        show();
     ++counter;
 }
 
-template<class Color>
+template <class Color>
 void Animation<Color>::run() {
     run(true);
 }
 
-template<class Color>
+template <class Color>
 void Animation<Color>::setLeds(unsigned int p1, unsigned int p2, Color color) {
-    if (p1 >= ledNum) p1 = ledNum - 1;
-    if (p2 >= ledNum) p2 = ledNum - 1;
-    if (p2 < p1) p2 = p1;
-    for (unsigned int p = p1; p <= p2; ++p) setLed(p, color);
+    if (p1 >= ledNum)
+        p1 = ledNum - 1;
+    if (p2 >= ledNum)
+        p2 = ledNum - 1;
+    if (p2 < p1)
+        p2 = p1;
+    for (unsigned int p = p1; p <= p2; ++p)
+        setLed(p, color);
 }
 
-template<class Color>
+template <class Color>
 void Animation<Color>::setLedsTransition(unsigned int p1, unsigned int p2, Color color1, Color color2) {
-    if (p2 < p1) p2 = p1;
-    int origLength{(int) (p2 - p1)};
-    if (p1 >= ledNum) p1 = ledNum - 1;
-    if (p2 >= ledNum) p2 = ledNum - 1;
-    int length{(int) (p2 - p1)};
+    if (p2 < p1)
+        p2 = p1;
+    int origLength{(int)(p2 - p1)};
+    if (p1 >= ledNum)
+        p1 = ledNum - 1;
+    if (p2 >= ledNum)
+        p2 = ledNum - 1;
+    int length{(int)(p2 - p1)};
     for (int i = 0; i <= length; ++i) {
-        double portion{i * 1.0 / origLength};
+        double portion{origLength > 0 ? i * 1.0 / origLength : 1.0};
         setLed(p1 + i, Color::mixColors(portion, color1, color2));
     }
-
 }
 
-
-template<class Color>
+template <class Color>
 void Animation<Color>::runTimed(unsigned long interval, unsigned long &lastRun) {
     unsigned long now = millis();
     bool shown = false;
@@ -139,13 +146,17 @@ void Animation<Color>::runTimed(unsigned long interval, unsigned long &lastRun) 
         Animation::run(false);
         shown = true;
         if (skipFrames) {
-            if (counter == 1) lastRun = now;
-            else lastRun += interval;
-            if (interval == 0) break;
+            if (counter == 1)
+                lastRun = now;
+            else
+                lastRun += interval;
+            if (interval == 0)
+                break;
         } else {
             lastRun = now;
             break;
         }
     }
-    if (shown && display->isShowOnRun()) show();
+    if (shown && display->isShowOnRun())
+        show();
 }
--- ColorRGB.h
+++ ColorRGB.h
@@ -2,20 +2,20 @@
 
 class ColorRGB {
 public:
+    constexpr ColorRGB() : r(0), g(0), b(0) {}
     constexpr ColorRGB(unsigned int r, unsigned int g, unsigned int b) : r(r), g(g), b(b) {}
 
     unsigned int r, g, b;
     static ColorRGB RED, GREEN, BLUE, WHITE, BLACK;
 
-    //mix two colors together, proportion decides the amount of each color (0..1) (full color1..full color2)
+    // mix two colors together, proportion decides the amount of each color (0..1) (full color1..full color2)
     constexpr static ColorRGB mixColors(double proportion, ColorRGB &color1, ColorRGB &color2);
 };
 
 constexpr ColorRGB ColorRGB::mixColors(double proportion, ColorRGB &color1, ColorRGB &color2) {
-    return ColorRGB{(unsigned int) (color1.r + ((int) color2.r - (int) color1.r) * proportion),
-                    (unsigned int) (color1.g + ((int) color2.g - (int) color1.g) * proportion),
-                    (unsigned int) (color1.b + ((int) color2.b - (int) color1.b) * proportion)
-    };
+    return ColorRGB{(unsigned int)(color1.r + ((int)color2.r - (int)color1.r) * proportion),
+                    (unsigned int)(color1.g + ((int)color2.g - (int)color1.g) * proportion),
+                    (unsigned int)(color1.b + ((int)color2.b - (int)color1.b) * proportion)};
 }
 
 ColorRGB operator*(ColorRGB &color, double value) {
